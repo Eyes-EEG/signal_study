69f3e3bf1cf23f5ca7fff87bc6ae1c3a4c015f91
sergio23p <76039944+sergio23p@users.noreply.github.com>
2021-08-09 13:03:19 +0100

    Delete openbcigui directory

--- openbcigui/main.py
+++
@@ -1,8 +0,0 @@
-from window_ui import *
-from MainWindow import MainWindow
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec_()
