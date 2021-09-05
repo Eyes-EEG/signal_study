103dd146f8fa6d379ae18f839e6a4dcdf55259ce
sergio23p <76039944+sergio23p@users.noreply.github.com>
2021-09-05 12:46:59 +0100

    Update main.m

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ listaFicheros = {'1_close_left_eye_1.csv', '1_close_left_eye_2.csv', '1_close_le
 markers = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
 
 myDataset = Dataset();
-myFeatures = Features();
+obj.Features = Features();
 
  for i = 1 : length(markers)
      myDataset = myDataset.addTrial(listaFicheros{i}, markers{i})   
@@ -12,6 +12,6 @@ myFeatures = Features();
 Datos_filtrados = {myDataset.data_filtered(1:160).filtered_data};
 
 Label = [ones(1,40) (repmat (2,1,40)) (repmat (3,1,40)) (repmat (4,1,40))];
-  for i = 1 : length(CH1)
-       myFeatures = myFeatures.CalculaValorAlfa(Datos_filtrados{i}, Label(i))   
+  for i = 1 : 160
+       [obj.features, featTable] = obj.features.generateModelFeatures (Datos_filtrados{i}, Label(i));   
   end
